09d52d39a631c10bc2d42e891e55b13145aeb81c
Swathi S <swathis@Swathis-MacBook-Air.local>
2023-03-31 18:50:42 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from './pages/home'
 import IncomePage from './pages/income'
 import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
+import SignInPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ForgetPasswordPage from './pages/ForgetPasswordPage'
 import MoneyManager from './elements/MoneyManager'
@@ -20,7 +20,7 @@ export default function App() {
                     <Route path="/" element={ <HomePage /> } />
                     <Route path="/incometracker" element={ <IncomePage /> } />
                     <Route path="/landing" element={ <LandingPage /> } />
-                    <Route path="/login" element={ <LoginPage /> } />
+                    <Route path="/login" element={ <SignInPage /> } />
                     <Route path="/register" element={ <RegisterPage /> } />
                     <Route path="/forget-password" element={ <ForgetPasswordPage /> } />
                 </Routes>
